c0cfd480a3814cacb021ba3ea49f3b6d28f66734
Junyi Ji <junyiji@gmail.com>
2020-11-29 20:14:11 +0100

    Create Question component, add props, add state

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import Question from './Question'
+import '../stylesheets/App.css';
+
+const totalQuestions= 10;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState('');
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState(true);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true); 
+
+  const checkAnswer = () => {
+
+  }
+
+  return (
+    <div className="App">
+      <Question questionNumber={questionNumber + 1} totalQuestions={totalQuestions} question={questions[questionNumber].question} answers={questions[questionNumber].answers} userAnswer={userAnswer ? userAnswer : undefined} callback={checkAnswer} />
+    </div>
+  );
+}
+
+export default App;
